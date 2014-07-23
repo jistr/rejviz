1da2436c280870e7edc286131e8ff23537d81295
Jiri Stransky <jistr@redhat.com>
2014-07-23 11:36:55 +0200

    Function to parse key-value args

---
+++ rejviz/utils.py
@@ -0,0 +1,24 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from six.moves import reduce
+
+
+def parse_keyvals(keyvals_string, item_separator=';', kv_separator='='):
+    keyvals_raw = keyvals_string.split(item_separator)
+
+    def keyvals_to_hash(keyvals, item):
+        key, value = item.split(kv_separator, 1)
+        keyvals[key] = value
+        return keyvals
+
+    return reduce(keyvals_to_hash, keyvals_raw, {})
